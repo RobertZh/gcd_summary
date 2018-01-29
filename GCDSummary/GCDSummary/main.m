bd57ee55672a928fce36bfff051830e272a87131
zhangwei <zhangwei8256@icloud.com>
2018-01-29 10:03:42 +0800

    新建空项目

---
+++ GCDSummary/GCDSummary/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GCDSummary
+//
+//  Created by zhangwei on 2018/1/29.
+//  Copyright © 2018年 zhangwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
